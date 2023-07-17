5df7d80db66a97eaddc5cda9df12e330fedcd0c9
Salvo Falcone <salvofalcone57@gmail.com>
2023-07-17 15:11:57 +0200

    chore: Esercizio 2 - uso useContext e createContext

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,28 @@
+import { createContext } from "react";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 import "@/styles/globals.css";
 
+export const GlobalContext = createContext({
+  user: {
+    name: "Salvo",
+    surname: "Falcone",
+    age: 25,
+  },
+  shoppingList: [
+    { id: 1, items: 24 },
+    { id: 2, items: 34 },
+    { id: 3, items: 44 },
+  ],
+});
+
 export default function App({ Component, pageProps }) {
   return (
     <div className="App">
       <Navbar />
-        <Component {...pageProps} />
+      <Component {...pageProps} />
       <Footer />
     </div>
   );
--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { GlobalContext } from "./_app";
+
 import Head from "next/head";
 
 import styles from "../styles/About.module.scss";
 
 const AboutUs = () => {
+  const context = useContext(GlobalContext);
+
   return (
     <>
       <Head>
@@ -13,7 +18,17 @@ const AboutUs = () => {
       </Head>
 
       <main className={styles.About}>
-        <h1>About Us section</h1>
+        <h1>Hello!</h1>
+
+        {/* test uso stato globale */}
+        <div>
+          <span>Contact </span>
+          <a href="https://www.linkedin.com/in/salvofalcone/">
+            <span>
+              {context.user.name} {context.user.surname}
+            </span>
+          </a>
+        </div>
       </main>
     </>
   );
